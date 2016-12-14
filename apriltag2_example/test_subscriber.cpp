d6da42bd9a60a757b4f2c0d64c0f0910041aeca9
Fabrizio Schiano <fabrizio.schiano@gmail.com>
2016-12-14 09:42:38 +0100

    I will work on another executable now. This was just to test a simple subscriber.

--- apriltag2_example/test_subscriber.cpp
+++ apriltag2_example/test_subscriber.cpp
@@ -1,33 +1,70 @@
+#include <boost/thread.hpp>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <message_filters/subscriber.h>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-    try
-    {
-        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-        cv::waitKey(30);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    }
+
+#include "test_subscriber.h"
+
+//namespace std;
+
+test_subscriber::test_subscriber(ros::NodeHandle &nh){
+    m_n = nh;
+
+    m_n.param<std::string>("cameraInfoName", m_cameraInfoName, "/camera/camera_info");
+    m_n.param( "frequency", freq, 30);
+    ROS_INFO("Subscribing to camera_info...");
+    m_cameraInfoSub = m_n.subscribe( m_cameraInfoName, 1, (boost::function < void(const sensor_msgs::CameraInfoConstPtr & )>) boost::bind( &test_subscriber::getCameraInfoCb, this, _1 ));
+    //    std::string str1 = new std::string;
+    ROS_INFO("...done");
+    ROS_INFO("Launch test_subscriber node ");
+
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "image_listener");
-    ros::NodeHandle nh;
-    cv::namedWindow("view");
-    cv::startWindowThread();
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, imageCallback);
 
-//    ros::Subscriber<sensor_msgs::CameraInfo> sub_cameraInfo(); // this is wrong
-//    message_filters::Subscriber<sensor_msgs::CameraInfo> sub_cameraInfo ; // I don't know how to finish this!
-    ros::spin();
-    cv::destroyWindow("view");
+test_subscriber::~test_subscriber(){
+    //    delete m_motionProxy;
+
+}
+
+
+void test_subscriber::getCameraInfoCb(const sensor_msgs::CameraInfoConstPtr &msg)
+{
+    ROS_INFO("Received Camera INFO");
+    // Convert the paramenter in the visp format
+    //  m_cam = visp_bridge::toVispCameraParameters(*msg);
+    //  m_cam.printParameters();
+
+
+  // Stop the subscriber (we don't need it anymore)
+  std::cout << "Stopping Camera INFO subscriber"<<std::endl;
+  this->m_cameraInfoSub.shutdown();
+
+  m_camInfoIsInitialized = 1;
+}
+
+
+void test_subscriber::spin()
+{
+    ros::Rate loop_rate(freq);
+    while(ros::ok()){
+
+        //    vpMouseButton::vpMouseButtonType button;
+        //    bool ret = vpDisplay::getClick(I, button, false);
+        //    this->computeHandPose();
+        //    if (m_mode_targetCalibration)
+        //    {
+        //      if (ret && button == vpMouseButton::button1 && m_statusPointArray)
+        //        this->computeTargetCalibration();
+
+        //      vpDisplay::flush(I);
+        //    }
+
+        //    ret = false;
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 }
